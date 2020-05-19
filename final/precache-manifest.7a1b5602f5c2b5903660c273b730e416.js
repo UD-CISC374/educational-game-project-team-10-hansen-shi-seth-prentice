5f9de0c995d2f4a1cff76596100aa958a7633789
Seth <sethp@udel.edu>
2020-05-18 23:45:11 -0400

    Updates

---
+++ final/precache-manifest.7a1b5602f5c2b5903660c273b730e416.js
@@ -0,0 +1,294 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e446921a9766226597313a1bd951a558",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/Enemies/bat.png"
+  },
+  {
+    "revision": "2edbe4e34b6761d1c545fc7e3239fca6",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/Enemies/bunny.png"
+  },
+  {
+    "revision": "858018643c352b49a704d436e461b7b1",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/Enemies/enemies.png"
+  },
+  {
+    "revision": "b15e93d0c18c5fe69f37a79b44b1e20f",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/Enemies/fire-ball.png"
+  },
+  {
+    "revision": "b9c482d07663ba6b5ec3f7a3e4d02eb6",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/Enemies/frog.png"
+  },
+  {
+    "revision": "c3344c729bc12ad455e594d9b516b481",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/Enemies/ghost.png"
+  },
+  {
+    "revision": "77d6c25eb7bcc7a4547a99b3d0240cc1",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/Enemies/mamaBun.png"
+  },
+  {
+    "revision": "9803b39e0ce8ecd8b7460d412535a356",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/Enemies/skeleton.png"
+  },
+  {
+    "revision": "ce5aa7c8dbf0184d5a038f806082884c",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/WizSheet.png"
+  },
+  {
+    "revision": "2a1dd7860f40ebd2c054644a4448b86e",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/background.jpg"
+  },
+  {
+    "revision": "79c4d0399232854c7cfe5eda929381c0",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/background.png"
+  },
+  {
+    "revision": "d6b8dd518194d9ac19ab43f7051dcaf3",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/citations.md"
+  },
+  {
+    "revision": "5e0ca6cd356b7a146f49740273d33d5b",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crys_1.png"
+  },
+  {
+    "revision": "4dfe2108f06b0d0cae5200e1d0989ce2",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crys_2.png"
+  },
+  {
+    "revision": "bf64604c351e45bb048f1b9bfac58d0b",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crys_3.png"
+  },
+  {
+    "revision": "91ebab6955520d1b5b18b9b076365f35",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crys_4.png"
+  },
+  {
+    "revision": "4e06d326011fa50b799a7fefece4c651",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crys_5.png"
+  },
+  {
+    "revision": "00794a35cfe1f6e67bfa51b931472cff",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crys_6.png"
+  },
+  {
+    "revision": "1a0f781b87be67577669ea4da86e865d",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crys_7.png"
+  },
+  {
+    "revision": "dfcc592e9cda053d8138eda145102e83",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crys_8.png"
+  },
+  {
+    "revision": "5fbe3f3217bd38e34ac300d2e489fa5f",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crys_9.png"
+  },
+  {
+    "revision": "c6800d69d0dd58b2fbebb5202c4693a9",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crystal_1.png"
+  },
+  {
+    "revision": "49ea8f3669e15e3e7da3e65644f22021",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crystal_2.png"
+  },
+  {
+    "revision": "bad10c40bbed701f918d7b9265f1ed87",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crystal_3.png"
+  },
+  {
+    "revision": "1bc34fb80ae768571d66f33ebdbfa8e4",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crystal_4.png"
+  },
+  {
+    "revision": "49cad01dbfaa61f95248f7678caa1af8",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crystal_5.png"
+  },
+  {
+    "revision": "efa3d69761eb533d62fc39d6f2c4aabb",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crystal_6.png"
+  },
+  {
+    "revision": "a602500bde120cbe4ffea0a7a0f37a5a",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crystal_7.png"
+  },
+  {
+    "revision": "c84e140e4bf6b7f4900949fab5f7d184",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crystal_8.png"
+  },
+  {
+    "revision": "a1048fd60ba1379e15c503b8e3f0373d",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/Crystal_9.png"
+  },
+  {
+    "revision": "01207d4d73b63f86414a18e157d14f73",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/emerald.png"
+  },
+  {
+    "revision": "a93954284433c2a4a3ff44adf9d0c4de",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/crystals/startingPouch.png"
+  },
+  {
+    "revision": "e62fe5077321bf47dcf5d1843d0b0fbf",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/environment-background.png"
+  },
+  {
+    "revision": "37fdf52ba0eabd5b3c1914361e7ae085",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/fightbutton.png"
+  },
+  {
+    "revision": "872f41e77eae3bfc4fba291b0145b12a",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/font.xml"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/fonts/font.png"
+  },
+  {
+    "revision": "4a568bc8d91dfb164751c71c30e9c5e3",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/heart.png"
+  },
+  {
+    "revision": "7bd1bba7c38909d15c1cc75d75753eed",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "7d6346640d56b56b95be1871ccb9babc",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "47f2657e2979f5bfee4cc8bb8ca304d8",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "14fbfdacec709de0c7265357ee38799e",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/icons/favicon.ico"
+  },
+  {
+    "revision": "14fbfdacec709de0c7265357ee38799e",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/icons/favicon2.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/icons/icons-192+1.png"
+  },
+  {
+    "revision": "99c7c9cc1a37c689cb37d67aa64cf116",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/icons/icons-512+1.png"
+  },
+  {
+    "revision": "0cd830f816f052eebeeb2ce6213af605",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/icons/site.webmanifest"
+  },
+  {
+    "revision": "d869b88726efc12efcda9316f17e5393",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/items.png"
+  },
+  {
+    "revision": "50bd96d65c54927743bcb58fbe2983e0",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/lava-fall.png"
+  },
+  {
+    "revision": "c882ef609dec12712880e8f2a0f5b527",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/lava.png"
+  },
+  {
+    "revision": "90cb2dd1f4549c9c5367cc37ed7f01c0",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/level/altar.png"
+  },
+  {
+    "revision": "8f5d9cc9330959adb0297bf22c87f4d2",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/level/batform.png"
+  },
+  {
+    "revision": "abb2a5902e4671a3850cdd72860a823f",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/level/battle.png"
+  },
+  {
+    "revision": "a05c072f69d9558a701b80e5911c40be",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/level/elevator.png"
+  },
+  {
+    "revision": "8f3be4c5476af3ba88801c7d5886e98a",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/level/level.png"
+  },
+  {
+    "revision": "2dbbd86589237154069032ce61f03db8",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/level/stairs.png"
+  },
+  {
+    "revision": "9acefb4bdd5f9b6c427ad4d2fbc834e5",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/level/statue.png"
+  },
+  {
+    "revision": "f135c3b69abb9a42ee22b17365aa17f2",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/maps/map.json"
+  },
+  {
+    "revision": "7d94245bf51773c4ce16dafa4f4926d5",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/platforms/map.png"
+  },
+  {
+    "revision": "246067bc7bfe342f624c3090be2a7c90",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/tileSets/environment-tiles.png"
+  },
+  {
+    "revision": "91b900562a5a8f6bd9be795aca8e9170",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/tileSets/tutorialTileSet.png"
+  },
+  {
+    "revision": "10d69c951816d3ee14070ecd1e9a370d",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/torch.png"
+  },
+  {
+    "revision": "eec84c6fa0c91fad2e1756baab812499",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/tutorialBattle.png"
+  },
+  {
+    "revision": "f019b7330b3cd1f078e2a7da1682d3ab",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/water.png"
+  },
+  {
+    "revision": "e3a8aef136c47e981de6180b3df4a80f",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalassets/waterfall.png"
+  },
+  {
+    "revision": "14fbfdacec709de0c7265357ee38799e",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalfavicon.ico"
+  },
+  {
+    "revision": "754aaba547fbc814ec7b",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalgen.main.4c75fd13240314c92bd2.bundle.js"
+  },
+  {
+    "revision": "633fc88d72846aef998d",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalgen.vendors.ebe8b6664d1bba7b6cb9.bundle.js"
+  },
+  {
+    "revision": "53f9d15ecaf118bdba13fb81d3ea7959",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-game-project-team-10-hansen-shi-seth-prentice/finalsw.js"
+  }
+]);
